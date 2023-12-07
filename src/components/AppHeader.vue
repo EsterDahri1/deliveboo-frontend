a1c7c694250f9c56bd1f8cf4c7567bcf54561aca
“LiciaLicari” <licianbsplicari@gmail.com>
2023-12-07 12:06:39 +0100

    add header + footer draft

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+   <h1>HEADER</h1>
+</template>
+
+<style lang="scss" scoped>
+svg {
+    fill: black;
+    font-size: 30px;
+}
+</style>
