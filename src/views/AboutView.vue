94df2763938cbe13341adb746a001e963012b314
EsterDahri <esterdahri@gmail.com>
2023-12-07 15:58:56 +0100

    add about us view

---
+++ src/views/AboutView.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+    name: 'AboutView',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>    
+ 
+
+<template>
+    <h1>Chi Siamo - App di Delivery</h1>
+
+    <img src="immagine_team.jpg" alt="Il nostro team" width="800" height="400">
+
+    <p>
+        <strong>Missione:</strong>
+        Siamo un team appassionato che si impegna a semplificare la tua esperienza di ordinare cibo. La nostra missione è
+        connettere le persone con i loro ristoranti preferiti, offrendo un servizio di delivery rapido, affidabile e
+        conveniente. Rendiamo accessibile il miglior cibo della tua città con un semplice tocco sul tuo dispositivo.
+    </p>
+
+    <p>
+        <strong>La Nostra Storia:</strong>
+        Fondata con la visione di trasformare il modo in cui le persone accedono al cibo, la nostra app di delivery è nata
+        dall'amore per la gastronomia e la tecnologia. Abbiamo iniziato come un piccolo gruppo di appassionati di cibo e
+        innovatori digitali con l'obiettivo di semplificare il processo di ordinazione, consentendo agli utenti di gustare i
+        loro piatti preferiti comodamente a casa propria.
+    </p>
+
+    <p>
+        <strong>Valori Fondamentali:</strong>
+        La trasparenza, l'affidabilità e la qualità sono i pilastri dei nostri valori. Ci impegniamo a offrire una
+        piattaforma di delivery che rispetti gli standard più elevati, garantendo che ogni esperienza di ordine sia
+        piacevole e senza problemi.
+    </p>
+
+    <p>
+        <strong>Innovazione Continua:</strong>
+        Siamo in costante evoluzione per soddisfare le esigenze mutevoli dei nostri utenti. Investiamo in tecnologie
+        all'avanguardia per migliorare costantemente la nostra app, offrendo funzionalità intuitive, tempi di consegna
+        ottimizzati e un'ampia gamma di opzioni gastronomiche.
+    </p>
+
+    <p>
+        <strong>Supporto alla Comunità Locale:</strong>
+        Siamo orgogliosi di sostenere la nostra comunità locale e i ristoranti indipendenti. Collaboriamo con una vasta
+        gamma di partner commerciali locali per promuovere la diversità culinaria e sostenere le piccole imprese.
+    </p>
+
+    <p>
+        Unendoci a noi, fai parte di una comunità che apprezza il buon cibo, la comodità e l'innovazione. Grazie per essere
+        parte della nostra storia e permetterci di portare il meglio della cucina direttamente a te.
+    </p>
+</template>
+
+<style lang="scss" scoped></style>
