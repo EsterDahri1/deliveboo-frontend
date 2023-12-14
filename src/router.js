b1a03a632d6fa99ad8cee7d9bd84d70951583025
RiccardoVoltolina <riky.volto@live.com>
2023-12-14 10:08:31 +0100

    refactoring code

--- src/router.js
+++ src/router.js
@@ -4,12 +4,6 @@ import AboutView from "./views/AboutView.vue";
 import RestaurantView from "./views/RestaurantView.vue";
 import PaymentView from "./views/PaymentView.vue";
 import CartView from "./views/CartView.vue";
-import CineseView from "./views/CineseView.vue";
-import AmericanoView from "./views/AmericanoView.vue";
-import GiapponeseView from "./views/GiapponeseView.vue";
-import MessicanoView from "./views/MessicanoView.vue";
-import CoreanoView from "./views/CoreanoView.vue";
-import ItalianoView from "./views/ItalianoView.vue";
 import FilterRestaurantsView from "./views/FilterRestaurantsView.vue";
 
 
@@ -39,36 +33,6 @@ const routes = [
         props: true  // Per passare il parametro come prop alla vista
     },
 
-    {
-        path: '/cinese',
-        name: 'cinese',
-        component: CineseView,
-    },
-    {
-        path: '/coreano',
-        name: 'coreano',
-        component: CoreanoView,
-    },
-    {
-        path: '/giapponese',
-        name: 'giapponese',
-        component: GiapponeseView,
-    },
-    {
-        path: '/italiano',
-        name: 'italiano',
-        component: ItalianoView,
-    },
-    {
-        path: '/messicano',
-        name: 'messicano',
-        component: MessicanoView,
-    },
-    {
-        path: '/americano',
-        name: 'americano',
-        component: AmericanoView,
-    },
 
     {
         path: '/about',
